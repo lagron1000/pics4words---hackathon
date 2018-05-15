390d6dd95a7f2c3438eb1260b220fc2d1c194a6c
lagron1000 <lagron1000@gmail.com>
2018-05-15 13:42:40 +0300

    ALL initial files added

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var fs = require('fs');
+var upload = multer();
