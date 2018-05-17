069306a405aeb3c0a67205c50f2050526bfd3997
lagron1000 <lagron1000@gmail.com>
2018-05-17 14:46:56 +0300

    done

--- public/main.js
+++ public/main.js
@@ -3,10 +3,12 @@ import renderer from './renderer.js';
 import eventsHandler from './eventsHandler.js'; 
 import daysApi from './ajaxApi.js'
 import wordApi from './wordApi.js'
+import oxfordApi from './oxfordApi.js'
 
 let DayApi = new daysApi();
-let WordApi = new wordApi()
-let dayRepository = new DaysRepository(DayApi, WordApi);
+let WordApi = new wordApi();
+let OxfordApi = new oxfordApi()
+let dayRepository = new DaysRepository(DayApi, WordApi, OxfordApi);
 let Renderer = new renderer();
 let EventsHandler = new eventsHandler(dayRepository, Renderer);
 
@@ -22,5 +24,5 @@ EventsHandler.registerDIsslikeImage();
 
 
 
-dayRepository.initData().then( () => {
+dayRepository.initData().then( () => { 
     Renderer.renderDay(dayRepository.images, dayRepository)});
--- public/oxfordApi.js
+++ public/oxfordApi.js
@@ -5,14 +5,14 @@ class oxfordApi{
     fetch(ranWord){
         return $.ajax({
             method: "GET",
-            url: "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + ranWord,
             Accept: "application/json",
             app_id: "ef28e814",
-            app_key: "ddbef48c66c711b9ad84e628504e730d"
+            app_key: "ddbef48c66c711b9ad84e628504e730d",
+            url: "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + ranWord,
         }).catch(function(data){
             console.log(data)
         })
     }
 }
 
-export default wordApi
+export default oxfordApi
